15d8287c37449f477aae945bffdfda403ecfa622
Margie Orellano <ing.margie.orellano@gmail.com>
2025-03-16 21:37:49 -0500

    Update DataBaseScript.sql

--- DataBaseScript.sql
+++ DataBaseScript.sql
@@ -1,35 +1,34 @@
-CREATE TABLE pokemon (
+CREATE TABLE pokemon ( --General information of Pokemon
     id NUMBER PRIMARY KEY,
-    nombre VARCHAR2(100) NOT NULL,
-    altura NUMBER,
-    peso NUMBER,
-    url_imagen VARCHAR2(255)
+    name VARCHAR2(100) NOT NULL,
+    height NUMBER,
+    weight NUMBER,
+    image_url VARCHAR2(255)
 );
 
-CREATE TABLE tipo (
+CREATE TABLE type ( --Types of pokemon (water, fire wind, etc )
     id NUMBER PRIMARY KEY,
-    nombre VARCHAR2(100) UNIQUE NOT NULL
+    name VARCHAR2(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE pokemon_tipo (
-    id_pokemon NUMBER,
-    id_tipo NUMBER,
-    PRIMARY KEY (id_pokemon, id_tipo),
-    FOREIGN KEY (id_pokemon) REFERENCES pokemon(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_tipo) REFERENCES tipo(id) ON DELETE CASCADE
+CREATE TABLE pokemon_type ( -- relation between pokemon and types 
+    pokemon_id NUMBER,
+    type_id NUMBER,
+    PRIMARY KEY (pokemon_id, type_id),
+    FOREIGN KEY (pokemon_id) REFERENCES pokemon(id) ON DELETE CASCADE,
+    FOREIGN KEY (type_id) REFERENCES type(id) ON DELETE CASCADE
 );
 
-CREATE TABLE habilidad (
+CREATE TABLE ability ( --list of abilities 
     id NUMBER PRIMARY KEY,
-    nombre VARCHAR2(100) UNIQUE NOT NULL
+    name VARCHAR2(100) UNIQUE NOT NULL,
+    description varchar(200)
 );
 
-CREATE TABLE pokemon_habilidad (
-    id_pokemon NUMBER,
-    id_habilidad NUMBER,
-    PRIMARY KEY (id_pokemon, id_habilidad),
-    FOREIGN KEY (id_pokemon) REFERENCES pokemon(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_habilidad) REFERENCES habilidad(id) ON DELETE CASCADE
+CREATE TABLE pokemon_ability ( --relation between pokemon and abilities
+    pokemon_id NUMBER,
+    ability_id NUMBER,
+    PRIMARY KEY (pokemon_id, ability_id),
+    FOREIGN KEY (pokemon_id) REFERENCES pokemon(id) ON DELETE CASCADE,
+    FOREIGN KEY (ability_id) REFERENCES ability(id) ON DELETE CASCADE
 );
-
-/
